069418f17e3c2a33ccf3b2fec5f267b523759d9f
HEMMOUDA Aymane <ahemmouda@gmail.com>
2023-05-18 18:49:54 +0100

    version 2 pre-release 8
    Working prototype of arrays and lists

--- Jeext/src/main/java/controllers/Index.java
+++ Jeext/src/main/java/controllers/Index.java
@@ -1,8 +1,16 @@
 package controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +21,7 @@ import jeext.controller.core.param.annotations.Name;
 import jeext.controller.core.param.consumers.annotations.*;
 import jeext.controller.core.param.validators.annotations.*;
 import jeext.dao.Manager;
+import jeext.util.exceptions.UnsupportedType;
 import models.User;
 import models.permission.Permission;
 
@@ -23,24 +32,67 @@ import models.permission.Permission;
 @WebController
 public class Index {
 	
+	public static void name(Integer   i) {
+		System.out.println("penis" +i);
+	}
+	
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+//		Method method = Index.class.getMethods()[1];
+//		Parameter parameter = method.getParameters()[0];
+//		System.out.println(parameter);
+//		System.out.println(parameter.getType());
+//		System.out.println(parameter.getParameterizedType());
+//		ParameterizedType t = (ParameterizedType) parameter.getParameterizedType();
+//		System.out.println(t.getActualTypeArguments().length);
+//		System.out.println(parameter.getParameterizedType().getTypeName());
+//		System.out.println(parameter.getType().getComponentType());
+//		System.out.println(parameter.getType().isArray());
+//		System.out.println(parameter.getType().arrayType());
+		
+		int [] i = new int [10];
+		Object  o  = i;
+		i = (int[]) o;
+		Object [] j = (Object[]) o;
+		
+//		List<Integer> l = List.of(1,2,69);
+//		method.invoke(null, l);
+		
+//		LocalTime t = LocalTime.now();
+//		LocalDateTime l = LocalDateTime.now();
+//		System.out.println(t);
+//		System.out.println(l);
+	}
+	
 	@WebMapping("/")
 	public static void hello (
-				@Required(false) @Default("world") String name,
-				@Required(false) @Default("12") Integer age,
-				@Required(false) @Default("1") @Name("id") User user ,
-				@Composed() User userr,
+//				@Required(false) @Default("world") String name,
+//				@Required(false) @Default("12") Integer age,
+//				@Required(false) @Default("1") @Name("id") User user ,
+				Integer age,
+				List<Integer> ages,
+//				List <Integer> ages_l,
+//				@Composed() User userr,
 				HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		request.setAttribute("name", name);
 //		
 //		request.getRequestDispatcher("/WEB-INF/jsps/index.jsp").forward(request, response);
 		
-		System.out.println("?");
-		user = Manager.find(User.class, "1");
+//		System.out.println("?");
+//		user = Manager.find(User.class, "1");
+		
+		System.out.println("Age: "+ age);
+		System.out.println("Ages: " +ages);
+		if (ages != null) {	
+			for (var i : ages) {
+				System.out.println("-> " +i);
+			}
+		}
 		
 //		Collections
-		List<E>
+//		List<E>
 		
-		Controller.writeSimpleText(response, "name: " +name +", age: " +age +"\nuser: "+user);
+		Controller.writeSimpleText(response, "out");
+//		Controller.writeSimpleText(response, "name: " +name +", age: " +age +"\nuser: "+user);
 	}
 	
 }
--- Jeext/src/main/java/jeext/controller/core/param/Param.java
+++ Jeext/src/main/java/jeext/controller/core/param/Param.java
@@ -19,8 +19,10 @@ import jeext.util.exceptions.UnsupportedType;
 import jeext.util.Parser;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,6 +43,7 @@ import java.util.List;
  * <li>{@link Character}
  * <li>{@link LocalDate}
  * </ul>
+ * And {@link Array}s and {@link List}s of the above mentioned types.
  * (Primitives aren't allowed, use their object representation instead)
  * <p>These {@link Param}s can be annotated with 3 types of {@link Annotation}
  * and 1 additional one for {@link Model}s only, these {@link Annotation}s are:
@@ -52,30 +55,44 @@ import java.util.List;
 public class Param {
 	// TODO add composer
 	private Class <?> type;
+	private Multiplicity multiplicity; 
 	/**
 	 * Primitive here means anything other than a Model.
 	 * Primitives themselves (int, float..) are not allowed
 	 */
 	private boolean primitive;
+	
+	
 	private String name;
 	private Validator [] validators;
 	private Consumer [] consumers;
+	// TODO check that only appropriate annotations are used on each type, like cant use min on model
+	// TODO if required then no default, if default then no required
+	// MENTION only models integer based id are allowed, no error now, but error when we try to retrieve, no UUID chief
 	// MENTION models should inherit from model to be considered a model, duh
 	// TODO matches validator throws regex error
 	// TODO default consumer for string cannot be empty string, cuz it consideres emptry string and null same
 	// TODO: check periodholder, i removed returning null now its just exception
-	@SuppressWarnings("unchecked")
-	public Param(Class <?> webController, Method method, Parameter parameter) {
+	
+	public Param (Class <?> webController, Method method, Parameter parameter) {
+		type = parameter.getType();
 		
-		this.type = parameter.getType();
+		if (type.isArray()) {
+			type = type.componentType();
+			multiplicity = Multiplicity.ARRAY;
+			
+		} else if (List.class.isAssignableFrom(type)) {
+			ParameterizedType parameterizedType = (ParameterizedType) parameter.getParameterizedType();
+			type = (Class <?>) parameterizedType.getActualTypeArguments()[0];
+			multiplicity = Multiplicity.LIST;
+			
+		} else {
+			multiplicity = Multiplicity.SINGLE;
+		}
 		
 		if (type.isPrimitive()) {
 			throw new InvalidMappingMethodParam(webController, method, parameter, "Primitives aren't allowed, use their Object representation instead");
 		}
-		
-		this.primitive = !Model.class.isAssignableFrom(type);
-		
-		this.name = (parameter.isAnnotationPresent(Name.class))? parameter.getAnnotation(Name.class).value() : parameter.getName();
 		
 		if (!(String.class.equals(type) ||
 				Number.class.isAssignableFrom(type) ||
@@ -84,9 +101,18 @@ public class Param {
 				Boolean.class.equals(type) ||
 				Character.class.equals(type))) {
 			throw new InvalidMappingMethodParam(webController, method, parameter, "Unsuported type `" +type +"`");
-		} 
+		}
 		
 		
+		primitive = !Model.class.isAssignableFrom(type);
+		name = (parameter.isAnnotationPresent(Name.class))? parameter.getAnnotation(Name.class).value() : parameter.getName();
+
+		
+		processValidators(webController, method, parameter);
+		processConsumers(webController, method, parameter);
+	}
+	
+	private void processValidators (Class <?> webController, Method method, Parameter parameter) {
 		List <Validator> _validators = new ArrayList <> ();
 		
 		if (!parameter.isAnnotationPresent(Required.class) || parameter.getAnnotation(Required.class).value()) {
@@ -230,8 +256,9 @@ public class Param {
 		}
 		
 		this.validators = _validators.toArray(new Validator [0]);
-		
-		
+	}
+	
+	private void processConsumers (Class <?> webController, Method method, Parameter parameter) {
 		List <Consumer> _consumers = new ArrayList <> ();
 		
 		if (String.class.equals(type)) {
@@ -330,18 +357,41 @@ public class Param {
 	}
 	
 	public Object getParam (HttpServletRequest request) throws InvalidParameter {
-		Object value;
+		Object value = switch (multiplicity) {
+		case SINGLE:
+			if (primitive) {
+				yield getParameter(name, type, request);
+			} else {
+				yield getEntity(name, type, request);
+			}
+			
+		case ARRAY:
+			if (primitive) {
+				yield getParameters(name, type, request);
+			} else {
+				yield getEntities(name, type, request);
+			}
+
+		case LIST:
+			Object array;
+			if (primitive) {
+				array = getParameters(name, type, request);
+			} else {
+				array = getEntities(name, type, request);
+			}
+			yield constructList(array, type);
+			
+		default:
+			throw new UnsupportedType(multiplicity);
+		};
 		
-		if (primitive) {
-			value = getParameter(name, type, request);
-		} else {
-			value = getEntity(name, type, request);
-		}
-		
-		validate(value);
-		
-		return consume(value);
+//		validate(value);
+//		
+//		return consume(value);
+		return value;
 	}
+	
+	
 	
 	private void validate (Object value) throws InvalidParameter {
 		for (Validator validator : validators) {
@@ -359,53 +409,101 @@ public class Param {
 		return value;
 	}
 
-	// TODO add arrays or lists, also check if u can instantiate an array of type clase <?>
-	private static Object getEntity (String name, Class <?> type, HttpServletRequest request) {
-		// FIXME AYAYYAYAYAYAYAYAYYAYAYAYAYYA 
-		Long id = (Long) getParameter(name, Long.class, request); // TODO ayay, something with objects instead. does this aslan work, cast null, yes casting null works
+	
+	@SuppressWarnings("unchecked")
+	private static <T> Object constructList (Object array, Class <T> type) {
+		List <T> list = Arrays.asList((T [])array);
+		return list;
+	}
+	
+	private static Object getEntity (String name, Class <?> clazz, HttpServletRequest request) {
+		Object id = getParameter(name, Long.class, request); // TODO If you have a suggestion as to how easily retrieve the models id type hmu
 		if (id == null) {
 			return null;
+		}
+		
+		return Manager.find(clazz, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T> Object getEntities (String name, Class <T> clazz, HttpServletRequest request) {
+		Long [] ids = (Long []) getParameters(name, Long.class, request); // TODO Same thing
+		if (ids == null) {
+			return null;
+		}
+		
+		T [] entities = (T []) Array.newInstance(clazz, ids.length);
+		for (int i = 0; i < ids.length; i++) {
+			Object id = ids[i];
+			entities[i] = (id == null)? null : Manager.find(clazz, id);
+		}
+		
+		return entities;
+	}
+	
+	private static Object getParameter (String name, Class <?> clazz, HttpServletRequest request) {
+		String parameter = request.getParameter(name);
+		return parse(parameter, clazz);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> Object getParameters (String name, Class <T> clazz, HttpServletRequest request) {
+		String [] parameters = request.getParameterValues(name);
+		if (parameters == null) {
+			return null;
+		}
+		
+		T [] parsedParameters = (T []) Array.newInstance(clazz, parameters.length);
+		for (int i = 0; i < parameters.length; i++) {
+			parsedParameters[i] = (T) parse(parameters[i], clazz);
+		}
+		
+		return parsedParameters;
+	}
+	
+	private static Object parse (String s, Class <?> clazz) {
+		if (s == null) {
+			return null;
+			
+		} else if (String.class.equals(clazz)) {
+			return s;
+			
+		} else if (Boolean.class.equals(clazz)) {
+			return Parser.parseBool(s);
+			
+		} else if (Integer.class.equals(clazz)) {
+			return Parser.parseInt(s);
+			
+		} else if (Float.class.equals(clazz)) {
+			return Parser.parseFloat(s);
+			
+		} else if (Double.class.equals(clazz)) {
+			return Parser.parseDouble(s);
+			
+		} else if (Long.class.equals(clazz)) {
+			return Parser.parseLong(s);
+			
+		} else if (LocalDate.class.equals(clazz)) {
+			return Parser.parseDate(s);
+			
+		} else if (Character.class.equals(clazz)) {
+			return Parser.parseChar(s);
+			
+		} else if (Short.class.equals(clazz)) {
+			return Parser.parseShort(s);
+			
+		} else if (Byte.class.equals(clazz)) {
+			return Parser.parseByte(s);
+			
 		} else {
-			return Manager.find(type, id);
+			throw new UnsupportedType(clazz);
 		}
 	}
 	
-	private static Object getParameter (String name, Class <?> type, HttpServletRequest request) {
-		String parameter = request.getParameter(name);
-		
-		if (parameter == null || String.class.equals(type)) {
-			return parameter;
-			
-		} else if (Boolean.class.equals(type)) {
-			return Parser.parseBool(parameter);
-			
-		} else if (Integer.class.equals(type)) {
-			return Parser.parseInt(parameter);
-			
-		} else if (Float.class.equals(type)) {
-			return Parser.parseFloat(parameter);
-			
-		} else if (Double.class.equals(type)) {
-			return Parser.parseDouble(parameter);
-			
-		} else if (Long.class.equals(type)) {
-			return Parser.parseLong(parameter);
-			
-		} else if (LocalDate.class.equals(type)) {
-			return Parser.parseDate(parameter);
-			
-		} else if (Character.class.equals(type)) {
-			return Parser.parseChar(parameter);
-			
-		} else if (Short.class.equals(type)) {
-			return Parser.parseShort(parameter);
-			
-		} else if (Byte.class.equals(type)) {
-			return Parser.parseByte(parameter);
-			
-		} else {
-			throw new UnsupportedType(type);
-		}
+	private static enum Multiplicity {
+		SINGLE,
+		ARRAY,
+		LIST;
 	}
 	
 }
